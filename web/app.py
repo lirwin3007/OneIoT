7f70d79256426b650f01526fe04d5c0733835749
Louis Irwin <coding@louisirwin.co.uk>
2019-04-26 22:53:39 +0100

    🚧 Added html for actions

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,10 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
     
-@app.route('/add_action')
+@app.route('/add_action_package')
 def add_action():
-    return render_template('add_action.html')    
+    return render_template('add_action_package.html')    
 
+@app.route('/is-assistant-running')
+def is_assistant_running():
+    try:
+        return "Running" if 'active (running)' in str(subprocess.check_output("systemctl status assistant.service", shell=True)) else "Halted"
+    except:
+        return "Halted"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
